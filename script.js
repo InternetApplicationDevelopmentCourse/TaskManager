de88f378737340d99e23355fbb271798a2c48636
Tal Mekler <tal.mekler11@gmail.com>
2023-05-13 22:41:08 +0300

    on Enter -> add task

--- script.js
+++ script.js
@@ -21,68 +21,80 @@ function updateSummary() {
   const numOfCompletedTasks = document.getElementsByClassName("checked").length;
   document.getElementById("completed-tasks").innerHTML = numOfCompletedTasks;
   const numOfUncompletedTasks = numOfTasks - numOfCompletedTasks;
-  document.getElementById("uncompleted-tasks").innerHTML = numOfUncompletedTasks;
+  document.getElementById("uncompleted-tasks").innerHTML =
+    numOfUncompletedTasks;
 }
 
 function onMoveUp(element) {
-    const task = element.parentElement.parentElement;
-    const prevTask = task.previousElementSibling;
-    if(prevTask){
-        task.parentElement.insertBefore(task, prevTask);
-    }
+  const task = element.parentElement.parentElement;
+  const prevTask = task.previousElementSibling;
+  if (prevTask) {
+    task.parentElement.insertBefore(task, prevTask);
+  }
 }
 
 function onMoveDown(element) {
+  const task = element.parentElement.parentElement;
+  const nextTask = task.nextElementSibling;
+  if (nextTask) {
+    task.parentElement.insertBefore(nextTask, task);
+  }
+}
+
+function onCheckedReposition(element) {
+  if (element.parentElement.classList.contains("checked")) {
     const task = element.parentElement.parentElement;
-    const nextTask = task.nextElementSibling;
-    if(nextTask){
-        task.parentElement.insertBefore(nextTask, task);
-    }
+    task.appendChild(element.parentElement);
+  } else {
+    const firstCheckedLi = document.querySelector(".checked");
+    element.parentElement.parentElement.insertBefore(
+      element.parentElement,
+      firstCheckedLi
+    );
+  }
 }
 
-function onCheckedReposition(element){
-    if(element.parentElement.classList.contains("checked")){
-        const task = element.parentElement.parentElement;
-        task.appendChild(element.parentElement);
-    }
-    else{
-        const firstCheckedLi = document.querySelector(".checked");
-        element.parentElement.parentElement.insertBefore(element.parentElement, firstCheckedLi);
-    }
-}
-
-function addTask(){
-    const newTask_text = document.querySelector(".add-task-container .add-task-input").value;
-    if (inputValidation(newTask_text)) {
-        const newTask = document.createElement("li");
-        newTask.classList.add("task");
-        newTask.innerHTML = `<button class="task-check-btn" onclick='onChecked(this)'></button>
+function addTask() {
+  const newTask_text = document.querySelector(
+    ".add-task-container .add-task-input"
+  ).value;
+  if (inputValidation(newTask_text)) {
+    const newTask = document.createElement("li");
+    newTask.classList.add("task");
+    newTask.innerHTML = `<button class="task-check-btn" onclick='onChecked(this)'></button>
         <p class="task-text">${newTask_text}</p>
         <div class="btns">
           <button class="delete-btn" onclick='onDelete(this)'>DELETE</button>
           <button class="up-btn move-task-btn" onclick='onMoveUp(this)'></button>
           <button class="down-btn move-task-btn" onclick='onMoveDown(this)'></button>
-        </div>`
-        const ul = document.querySelector("ul.tasks");
-        const firstCheckedLi = document.querySelector(".checked");
-        if(firstCheckedLi == null){
-            ul.appendChild(newTask);
-        }
-        else{
-            ul.insertBefore(newTask, firstCheckedLi);
-        }
-        document.querySelector(".add-task-container .add-task-input").value = "";
-        updateSummary();
+        </div>`;
+    const ul = document.querySelector("ul.tasks");
+    const firstCheckedLi = document.querySelector(".checked");
+    if (firstCheckedLi == null) {
+      ul.appendChild(newTask);
+    } else {
+      ul.insertBefore(newTask, firstCheckedLi);
     }
+    document.querySelector(".add-task-container .add-task-input").value = "";
+    updateSummary();
+  }
 }
 
-function onChecked(element){
-    element.parentElement.classList.toggle("checked");
-    onCheckedReposition(element);
-    updateSummary();
+function onChecked(element) {
+  element.parentElement.classList.toggle("checked");
+  onCheckedReposition(element);
+  updateSummary();
 }
 
-function onDelete(element){
-    element.parentElement.parentElement.remove();
-    updateSummary();
+function onDelete(element) {
+  element.parentElement.parentElement.remove();
+  updateSummary();
 }
+
+//on enter key press
+document.querySelector(".add-task-container .add-task-input").addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.querySelector(".add-task-container .add-task-btn").click();
+    }
+  });
