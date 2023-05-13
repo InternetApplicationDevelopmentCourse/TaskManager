e9de83ed82a1416789a0dd4262992e261c14131a
Tal Mekler <tal.mekler11@gmail.com>
2023-05-13 18:57:40 +0300

    addTask()

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-function inputValidation() {
-  const input_value = document.querySelector(
-    ".add-task-container .add-task-input"
-  ).value;
+function inputValidation(input_value) {
   const error_msg = document.querySelector(".error-msg");
   if (input_value == "") {
     error_msg.querySelector("p").innerText = "Error: you didn't enter a task";
@@ -25,4 +22,23 @@ function updateSummary() {
   document.getElementById("completed-tasks").innerHTML = numOfCompletedTasks;
   const numOfUncompletedTasks = numOfTasks - numOfCompletedTasks;
   document.getElementById("uncompleted-tasks").innerHTML = numOfUncompletedTasks;
+}
+
+function addTask(){
+    const newTask_text = document.querySelector(".add-task-container .add-task-input").value;
+    if (inputValidation(newTask_text)) {
+        const newTask = document.createElement("li");
+        newTask.classList.add("task");
+        newTask.innerHTML = `<button class="task-check-btn"></button>
+        <p class="task-text">${newTask_text}</p>
+        <div class="btns">
+          <button class="delete-btn">DELETE</button>
+          <button class="up-btn move-task-btn"></button>
+          <button class="down-btn move-task-btn"></button>
+        </div>`
+        document.querySelector("ul.tasks").appendChild(newTask);
+        document.querySelector(".add-task-container .add-task-input").value = "";
+        updateSummary();
+    }
+    
 }
